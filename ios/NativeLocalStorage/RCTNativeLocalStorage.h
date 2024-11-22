f2e2783949088656608ac02b71c0172b81587541
Jeff Swartz <jeff.swartz@vonage.com>
2024-11-22 15:23:02 -0800

    Add steps from tutorial
    See https://reactnative.dev/docs/turbo-native-modules-introduction

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by Swartz on 11/17/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
